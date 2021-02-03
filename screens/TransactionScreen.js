6b7b3d38327303a35fb71f4bb2965acd4ad9ffe1
smitipachnanda <pachnandasmiti@gmail.com>
2021-02-03 17:19:23 +0530

    QR scanner

--- screens/TransactionScreen.js
+++ screens/TransactionScreen.js
@@ -6,57 +6,58 @@ import {BarCodeScanner} from 'expo-barcode-scanner'
 export default class TransactionScreen extends React.Component{
     constructor(){
         super();
-        this.state={
+        this.state = {
             hasCameraPermissions:null,
-            scanned:false,
-            scannedData:'',
+            scanned: false,
+            scannedData:'', 
             buttonState:'normal'
         }
     }
 
     getCameraPermissions = async()=>{
-        const {status} = await Permissions.askAsync(Permissions.CAMERA);
+        const {status} = await Permissions.askAsync(Permissions.CAMERA)
         this.setState({
-            /* status===granted is true when the user has granted permission
-            status=== granted is false if the user has not granted the permission
-            */
-           hasCameraPermissions: status === 'granted',
-           buttonState:'clicked'
-        })
+        /*status==='granted' is true when the user grants the permission
+        status ==='granted' is false if user does not grant permission*/
+        hasCameraPermissions:status==='granted' })
     }
 
     handleBarCodeScanned = async({type,data})=>{
-            this.setState({
-                scanned:true,
-                scannedData:data,
-                buttonState:'normal'
-            })
+        this.setState({
+            scanned:true,
+            scannedData:data,
+            buttonState:'normal'
+        })
     }
+
+    
     render(){
-        const hasCameraPermissions = this.state.hasCameraPermissions
-        const scanned = this.state.scannedData;
+        const hasCameraPermissions = this.state.hasCameraPermissions;
+        const scanned = this.state.scanned;
         const buttonState = this.state.buttonState;
 
-        if(buttonState === 'clicked' & hasCameraPermissions){
+        if(buttonState ==='clicked' & hasCameraPermissions){
             return(
                 <BarCodeScanner 
-                onBarCodeScanned = {scanned ? undefined : this.handleBarCodeScanned} />
+                onBarCodeScanned = {scanned? undefined : this.handleBarCodeScanned}
+                />
             )
         }
-        else if(buttonState === 'normal'){
-        return(
+        else if(buttonState)
+                return(
             <View style={styles.container}>
-                <Text style={styles.displayText}>Dummy QR Code output </Text>{
-                hasCameraPermissions === true ? this.state.scannedData:'Request camera permission'}
-                <TouchableOpacity style={styles.scanButton}
+                <Text style={styles.displayText}>{
+                hasCameraPermissions===true ? this.state.scannedData:'Request Camera Permission'
+                }</Text>
+                <TouchableOpacity style={styles.scanutton}
                 onPress = {this.getCameraPermissions}>
-                    <Text>Scan QR Code</Text>
+                <Text>Scan QR Code</Text>
                 </TouchableOpacity>
             </View>
         )
     }
 }
-}
+
 
 const styles = StyleSheet.create(
     {
@@ -79,5 +80,3 @@ const styles = StyleSheet.create(
         }
     }
 )
-
-//QR Scaner Code
